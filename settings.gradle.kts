b18c13f9cffeb2070f3e0539f9d4f7dbed3b89a3
xEricL <37921711+xEricL@users.noreply.github.com>
2025-08-26 01:54:48 -0400

    refactor: gradle scripts (#2)
    * add .gitattributes
    
    * upgrade gradle to 9.0.0
    
    * upgrade shadow to 9.0.2
    
    * implement build-logic/conventions
    
    * add license headers via spotless
    
    * ci: add workflow to auto publish builds
    
    * docs: tidy up README
    
    * 1.0.5-SNAPSHOT

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,40 @@
+@file:Suppress("UnstableApiUsage")
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        maven("https://lib.alpn.cloud/releases/")
+        maven("https://lib.alpn.cloud/snapshots/")
+        maven("https://lib.alpn.cloud/mirrors/")
+        mavenLocal()
+    }
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+}
+
+
+pluginManagement {
+    includeBuild("build-logic")
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version ("1.0.0")
+}
+
 rootProject.name = "Bearconomy"
 
-include("plugin-api")
-include("plugin-common")
+sequenceOf(
+    "api",
+    "common",
+).forEach {
+    val name = "bearconomy-$it"
+    include(name)
+    project(":${name}").projectDir = file("plugin-${it}")
+}
+
 include("example")
